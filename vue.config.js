2e7c3a2aa6429ddec7c752f7da5a36cf0fda04ea
marande <angelesdelgadillo99@gmail.com>
2024-11-13 20:57:35 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/GestorTareas/' // Asegúrate de que "GestorTareas" sea el nombre exacto de tu repositorio.
+    ? '/GestorTareas/'
     : '/'
 });
